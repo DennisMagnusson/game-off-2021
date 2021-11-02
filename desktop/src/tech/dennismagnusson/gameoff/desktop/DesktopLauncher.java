1f964dcb6e9432d51061a4594341ac2dfa4716c9
Dennis Magnusson <denmag@kth.se>
2021-11-02 12:14:48 +0100

    Add some movement

--- desktop/src/tech/dennismagnusson/gameoff/desktop/DesktopLauncher.java
+++ desktop/src/tech/dennismagnusson/gameoff/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import tech.dennismagnusson.gameoff.MainGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
+		config.foregroundFPS = 60;
 		new LwjglApplication(new MainGame(), config);
 	}
 }
